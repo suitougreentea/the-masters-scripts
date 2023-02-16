116360ef6d875e10ef0501751f862b9acc9fff47
suitougreentea <suitougreentea@users.noreply.github.com>
2023-02-16 21:58:35 +0900

    prettify

--- competition-sheet/src/client/sidebar.ts
+++ competition-sheet/src/client/sidebar.ts
@@ -5,21 +5,6 @@ let currentStageIndex = -1;
 const page0Container = document.querySelector<HTMLDivElement>("#page0")!;
 const page1Container = document.querySelector<HTMLDivElement>("#page1")!;
 
-const competitionSettingsForm = document.querySelector<HTMLFormElement>("#competition-settings")!;
-competitionSettingsForm.onsubmit = (_) => {
-  withLoader(async () => {
-    await runServerScript("setupCompetition", [competitionSettingsForm]);
-    currentStageIndex += 1;
-    await getAndApplyStageInfo(currentStageIndex);
-  });
-};
-
-const manualCheckbox = competitionSettingsForm.querySelector<HTMLInputElement>("input[name=manual]")!;
-const manualNumberOfGamesInput = competitionSettingsForm.querySelector<HTMLInputElement>("input[name=manualNumberOfGames]")!;
-manualCheckbox.onchange = (_) => {
-  manualNumberOfGamesInput.disabled = !manualCheckbox.checked;
-};
-
 const stageNameSpan = document.querySelector<HTMLSpanElement>("#stage-name")!;
 const prevStageButton = document.querySelector<HTMLButtonElement>("#prev-stage")!;
 const nextStageButton = document.querySelector<HTMLButtonElement>("#next-stage")!;
@@ -35,8 +20,8 @@ async function getAndApplyStageInfo(stageIndex: number) {
 
     stageNameSpan.innerText = `[${stageIndex + 1}] ${result.stageName}`;
     setPlayerNames(result.players.map(e => e != null ? e.name : null));
-    applyPlayersOrderButton.disabled = result.manual || result.wildcard;
-    applyResultButton.disabled = false;
+    applyPlayersOrderButton.disabled = result.wildcard;
+    applyResultButton.disabled = result.manual;
   } catch {
     stageNameSpan.innerText = `[${stageIndex + 1}] -`;
     setPlayerNames([null, null, null, null, null, null, null, null]);
@@ -45,42 +30,62 @@ async function getAndApplyStageInfo(stageIndex: number) {
   }
 }
 
+async function changeStage(newIndex: number) {
+  currentStageIndex = newIndex;
+  if (currentStageIndex == -1) {
+    stageNameSpan.innerText = "[0] ホーム";
+    page0Container.style.display = "block";
+    page1Container.style.display = "none";
+    refreshStageButton.disabled = true;
+  } else {
+    await getAndApplyStageInfo(currentStageIndex);
+    page0Container.style.display = "none";
+    page1Container.style.display = "block";
+    refreshStageButton.disabled = false;
+  }
+}
+
 prevStageButton.onclick = (_) => {
   withLoader(async () => {
-    if (currentStageIndex == 0) {
-      page0Container.style.display = "block";
-      page1Container.style.display = "none";
-      stageNameSpan.innerText = "[0] ホーム";
-      currentStageIndex = -1;
-    } else if (currentStageIndex > 0) {
-      currentStageIndex -= 1;
-      await getAndApplyStageInfo(currentStageIndex);
+    if (currentStageIndex >= 0) {
+      await changeStage(currentStageIndex - 1);
     }
   });
 };
 
 nextStageButton.onclick = (_) => {
   withLoader(async () => {
-    if (currentStageIndex == -1) {
-      page0Container.style.display = "none";
-      page1Container.style.display = "block";
-    }
-    currentStageIndex += 1;
-    await getAndApplyStageInfo(currentStageIndex);
+    await changeStage(currentStageIndex + 1);
   });
 };
 
 refreshStageButton.onclick = (_) => {
   withLoader(async () => {
-    await getAndApplyStageInfo(currentStageIndex);
+    if (currentStageIndex >= 0) {
+      await getAndApplyStageInfo(currentStageIndex);
+    }
   });
 };
 
-Sortable.create(document.querySelector("#players-sort>.players-sort-sortable")!);
+const competitionSettingsForm = document.querySelector<HTMLFormElement>("#competition-settings")!;
+competitionSettingsForm.onsubmit = (_) => {
+  withLoader(async () => {
+    await runServerScript("setupCompetition", [competitionSettingsForm]);
+    await changeStage(0);
+  });
+};
+
+const manualCheckbox = competitionSettingsForm.querySelector<HTMLInputElement>("input[name=manual]")!;
+const manualNumberOfGamesInput = competitionSettingsForm.querySelector<HTMLInputElement>("input[name=manualNumberOfGames]")!;
+manualCheckbox.onchange = (_) => {
+  manualNumberOfGamesInput.disabled = !manualCheckbox.checked;
+};
+
+Sortable.create(document.querySelector("#players-sort-sortable")!);
 
 // 並べ替えた順番になったノードを毎回取得する
 function getPlayersSortableItems(): HTMLDivElement[] {
-  return Array.from(document.querySelectorAll<HTMLDivElement>("#players-sort>.players-sort-sortable>.item"));
+  return Array.from(document.querySelectorAll<HTMLDivElement>("#players-sort-sortable>.item"));
 }
 
 function setPlayerNames(names: (string | null)[]) {
@@ -110,3 +115,5 @@ applyResultButton.onclick = (_) => {
     await getAndApplyStageInfo(currentStageIndex);
   });
 };
+
+changeStage(-1);
