70ba710fb1fd1123bed69de1f130936eba21c6ca
suitougreentea <suitougreentea@users.noreply.github.com>
2024-03-12 22:25:35 +0900

    fallback-timer: now working

---
+++ fallback-timer/build_page.ts
@@ -0,0 +1,21 @@
+import { denoPlugins } from "https://deno.land/x/esbuild_deno_loader@0.9.0/mod.ts";
+import { build } from "https://deno.land/x/esbuild@v0.20.0/mod.js";
+
+const result = await build({
+  bundle: true,
+  write: false,
+  charset: "utf8",
+  entryPoints: ["page.ts"],
+  outfile: "page.js",
+  target: "es2017",
+  plugins: [
+    ...denoPlugins(),
+  ],
+});
+
+const script = result.outputFiles[0].text;
+const html = await Deno.readTextFile("page.html");
+const css = await Deno.readTextFile("page.css");
+
+const outHtml = html.replace(`import "./page.ts";`, script).replace(`@import url("./page.css");`, css);
+await Deno.writeTextFile("dist/page.html", outHtml);
--- fallback-timer/main.ts
+++ fallback-timer/main.ts
@@ -1,9 +1,44 @@
+import { spreadsheetValueToTime } from "../common/spreadsheet_util.ts";
+
 declare let global: Record<string, (...args: any[]) => void>
 
-export function add(a: number, b: number): number {
-  return a + b;
+global.doGet = (_: GoogleAppsScript.Events.DoGet) => {
+  return HtmlService.createHtmlOutputFromFile("page");
 }
 
-global.test = () => {
-  console.log(add(1, 3));
+global.getData = (inputAsJson: string): string => {
+  try {
+    const input = JSON.parse(inputAsJson);
+    const { spreadsheet, game } = input;
+    const result = handleApi(spreadsheet, game);
+    return JSON.stringify({ result });
+  } catch (e) {
+    if (e instanceof Error) {
+      return JSON.stringify({ error: e.message });
+    } else {
+      return JSON.stringify({ error: e });
+    }
+  }
 }
+
+export type TimerData = {
+  name: string,
+  startTime: number,
+}[];
+
+const handleApi = (spreadsheetUrl: string, gameIndex: number) => {
+  /*
+  const urlMatch = spreadsheetUrl.match(/^https:\/\/docs\.google\.com\/spreadsheets\/d\/(.*)\/.*$/);
+  if (!urlMatch) throw new Error("Invalid URL");
+  */
+  const spreadsheet = SpreadsheetApp.openByUrl(spreadsheetUrl);
+  const sheet = spreadsheet.getSheetByName("Competition")!;
+
+  const range = sheet.getRange(3 + gameIndex * 11, 8, 8, 4);
+  const data = range.getValues().map(row => ({
+    name: String(row[0]),
+    startTime: spreadsheetValueToTime(row[3]),
+  }));
+  
+  return data;
+};
